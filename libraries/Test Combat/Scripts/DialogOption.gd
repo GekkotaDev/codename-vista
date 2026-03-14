396d4741a36d7a875d8da99219b82ea58c4fedfa
GekkotaDev <114091010+GekkotaDev@users.noreply.github.com>
2026-03-14 16:23:49 +0800

    Revert "Added combat tests"

--- libraries/Test Combat/Scripts/DialogOption.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name DialogOption
-
-@export var text: String = ""
-@export var npc_reply: Array[String] = []
-
-# This allows for infinite branching dialog
-@export var next_options: Array[DialogOption] = []
